332295e4bea9aef7abf9c37a8ccd1ab308c7b23d
sk.kwon <sk.kwon@battleent.com>
2018-05-18 09:21:05 +0900

    Symbol rename and refactoring

--- contracts/token/PXL.sol
+++ contracts/token/PXL.sol
@@ -4,12 +4,12 @@ import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./OwnableToken.sol";
 
-contract PIXL is StandardToken, OwnableToken {
+contract PXL is StandardToken, OwnableToken {
     using SafeMath for uint256;    
     
     // Token Information
     string public constant name = "Pixel";
-    string public constant symbol = "PIXL";
+    string public constant symbol = "PXL";
     uint256 public constant decimals = 18;
     uint256 public totalSupply;
 
@@ -20,7 +20,7 @@ contract PIXL is StandardToken, OwnableToken {
     constructor(uint256 initialSupply) public {                        
         require(initialSupply > 0);
 
-        totalSupply = initialSupply.mul(10 ** uint256(decimals));
+        totalSupply = initialSupply;
         balances[msg.sender] = totalSupply;
     }
 
@@ -43,15 +43,13 @@ contract PIXL is StandardToken, OwnableToken {
 //////////////////////
     function mint(address _to,uint256 _amount) onlyOwner public returns (bool) {
         require(_to != address(0));
-        require(_amount >= 0);
+        require(_amount >= 0);        
 
-        uint256 amount = _amount.mul(10 ** uint256(decimals));
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
 
-        totalSupply = totalSupply.add(amount);
-        balances[_to] = balances[_to].add(amount);
-
-        Mint(_to, amount);
-        Transfer(address(0), _to, amount);
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
 
         return true;
     }
@@ -60,11 +58,11 @@ contract PIXL is StandardToken, OwnableToken {
         require(_amount >= 0);
         require(_amount <= balances[msg.sender]);
 
-        totalSupply = totalSupply.sub(_amount.mul(10 ** uint256(decimals)));
-        balances[msg.sender] = balances[msg.sender].sub(_amount.mul(10 ** uint256(decimals)));
+        totalSupply = totalSupply.sub(_amount);
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
 
-        Burn(msg.sender, _amount.mul(10 ** uint256(decimals)));
-        Transfer(msg.sender, address(0), _amount.mul(10 ** uint256(decimals)));
+        Burn(msg.sender, _amount);
+        Transfer(msg.sender, address(0), _amount);
     }
 
     event Mint(address indexed _to, uint256 _amount);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const env = require("./../env");
+const PXL = artifacts.require("PXL");
+const decimals = Math.pow(10, 18);
 
-module.exports = function(deployer, network) {
-    const contract = artifacts.require("PIXL");
-    if (network == 'testnet_token') {
-    	deployer.deploy(contract, 10000);   
+module.exports = async (deployer, network) => {
+    if (network == 'testnet_token') {        
+        var token = await PIXL.new(10000 * decimals);
+        token.transfer(env.testnet.developersAddress, 5000 * decimals);
     } else if (network == 'mainnet_token') {
         deployer.deploy(contract, 10000);
     }
