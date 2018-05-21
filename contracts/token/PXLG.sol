887d059ad06677c791931b6c5b60a6390d289940
charls.kim <cs.kim@battleent.com>
2018-05-21 18:19:02 +0900

    Add PXL Genesis contract

---
+++ contracts/token/PXLG.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./OwnableToken.sol";
+
+contract PXLG is StandardToken, OwnableToken {
+    using SafeMath for uint256;    
+    
+    // Token Information
+    string public constant name = "Pixel Genesis";
+    string public constant symbol = "PXLG";
+
+    uint256 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    // token is non-transferable until owner calls unlock()
+    // (to prevent OTC before the token to be listed on exchanges)
+    bool isTransferable = false;
+
+    constructor(uint256 initialSupply) public {                        
+        require(initialSupply > 0);
+
+        totalSupply = initialSupply;
+        balances[msg.sender] = totalSupply;
+
+        Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function () public payable {
+		revert();
+    }
+
+    function unlock() external onlyOwner {
+        isTransferable = true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(isTransferable || owners[msg.sender]);
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(isTransferable || owners[msg.sender]);
+        return super.transfer(_to, _value);
+    }
+
+//////////////////////
+//  mint and burn   //
+//////////////////////
+    function mint(address _to,uint256 _amount) onlyOwner public returns (bool) {
+        require(_to != address(0));
+        require(_amount >= 0);        
+
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
+
+        return true;
+    }
+
+    function burn(uint256 _amount) onlyOwner public {
+        require(_amount >= 0);
+        require(_amount <= balances[msg.sender]);
+
+        totalSupply = totalSupply.sub(_amount);
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+
+        Burn(msg.sender, _amount);
+        Transfer(msg.sender, address(0), _amount);
+    }
+
+    event Mint(address indexed _to, uint256 _amount);
+    event Burn(address indexed _from, uint256 _amount);
+}
