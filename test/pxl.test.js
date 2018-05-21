8d91d2c7da7d4e8f3c85aa73cce0e94f22510ab4
jhSeo <jh.seo@battleent.com>
2018-05-21 16:23:51 +0900

    Convert token test to chai style

--- test/pxl.test.js
+++ test/pxl.test.js
@@ -1,37 +1,43 @@
 var PXL = artifacts.require("PXL");
 
-contract('PXL', function(accounts) {
+const BigNumber = web3.BigNumber;
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+
+contract('PXL', function (accounts) {
   const owner = accounts[0];
   const developer = accounts[1];
 
   const decimals = Math.pow(10, 18);
-  const initialBalance = 10000;
+  const initialBalance = 10000 * decimals;
+  const sendPXL = 3000 * decimals;
 
   let token;
 
-  beforeEach("setup contract for each test", async function() {
-    token = await PXL.new(initialBalance * decimals, { from: owner });
-  })
-
-  it("Check Initial balance in Owner account.", async() => {
-    let balance = await token.balanceOf.call(owner);
-    console.log("Owner address :", owner);
-    assert.equal(balance.valueOf(), initialBalance * decimals, "Owner's balance wasn't correct.");
+  beforeEach("setup contract for each test", async () => {
+    token = await PXL.new(initialBalance, { from: owner });
   });
 
-  it("Send 3000 pixel to developer account.", async() => {
-    let sendPXL = 3000 * decimals;
-    let beforeOwnerBalance = await token.balanceOf.call(owner);
-    token.transfer(developer, sendPXL);
+  it("Check Initial balance in Owner account.", async () => {
+    const balance = await token.balanceOf.call(owner);
+    console.log("Owner address :", owner);
+    balance.should.be.bignumber.equal(initialBalance);
+  });
 
-    let afterOwnerBalance = await token.balanceOf.call(owner);
-    let devBalance = await token.balanceOf.call(developer);
+  it("Send 3000 pixel to developer account.", async () => {
+    const before = await token.balanceOf.call(owner);
+    await token.transfer(developer, sendPXL);
+    const after = await token.balanceOf.call(owner);
+    const devBalance = await token.balanceOf.call(developer);
 
     console.log("Owner address :", owner);
     console.log("developer address :", developer);
 
-    assert.equal(afterOwnerBalance.valueOf(), beforeOwnerBalance - sendPXL, "Owner's balance wasn't correct.");
-    assert.equal(devBalance.valueOf(), sendPXL, "Developer's balance wasn't correct.");
+    before.minus(after).should.be.bignumber.equal(sendPXL);
+    devBalance.should.be.bignumber.equal(sendPXL);
   });
 });
 
