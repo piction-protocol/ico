bdb39f3db1e3867be3cd089db41fa0e9bcb92368
sk.kwon <sk.kwon@battleent.com>
2018-05-21 20:55:20 +0900

    Refactoring migrations

---
+++ migrations/2_deploy_pxlg_contracts.js
@@ -0,0 +1,10 @@
+const colors = require('colors');
+const PXLG = artifacts.require("PXLG");
+const decimals = Math.pow(10, 18);
+
+module.exports = async (deployer, network) => {	
+    var contract = await PXLG.new(process.env.TOTAL_SUPPLY_PXLG * decimals);
+    console.log(colors.red('Deployed PXLG contract'));
+	console.log(colors.red('InitialSupply : %sPXLG'), process.env.TOTAL_SUPPLY_PXLG);
+    console.log(colors.red('PXLG address : %s'), contract.address);
+};
