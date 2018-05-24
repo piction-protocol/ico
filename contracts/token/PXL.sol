d2ed862dcbaef4e4f8fd116f45707172032c0f7b
makesource <makesource@gmail.com>
2018-05-24 16:08:48 +0900

    Add PXL Contract documentation

--- contracts/token/PXL.sol
+++ contracts/token/PXL.sol
@@ -4,19 +4,30 @@ import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./OwnableToken.sol";
 
+/**
+ * @title PXL implementation based on StandardToken ERC-20 contract.
+ *
+ * @author Charls Kim - <cs.kim@battleent.com>
+ * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ */
 contract PXL is StandardToken, OwnableToken {
     using SafeMath for uint256;
 
-    // Token Information
+    // Token basic information
     string public constant name = "Pixel";
     string public constant symbol = "PXL";
     uint256 public constant decimals = 18;
     uint256 public totalSupply;
 
-    // token is non-transferable until owner calls unlock()
+    // Token is non-transferable until owner calls unlock()
     // (to prevent OTC before the token to be listed on exchanges)
     bool isTransferable = false;
 
+    /**
+     * @dev PXL constrcutor
+     *
+     * @param initialSupply Initial PXL token supply to issue.
+     */
     constructor(uint256 initialSupply) public {
         require(initialSupply > 0);
 
@@ -24,23 +35,49 @@ contract PXL is StandardToken, OwnableToken {
         balances[msg.sender] = totalSupply;
     }
 
+    /**
+     * @dev unlock PXL transfer
+     *
+     * @notice token contract is initially locked.
+     * @notice contract owner should unlock to enable transaction.
+     */
     function unlock() external onlyOwner {
         isTransferable = true;
     }
 
+    /**
+     * @dev Transfer tokens from one address to another
+     * 
+     * @notice override transferFrom to block transaction when contract was locked.
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     * @return A boolean that indicates if transfer was successful.
+     */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(isTransferable || owners[msg.sender]);
         return super.transferFrom(_from, _to, _value);
     }
 
+    /**
+     * @dev Transfer token for a specified address
+     *
+     * @notice override transfer to block transaction when contract was locked.
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     * @return A boolean that indicates if transfer was successful.
+     */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(isTransferable || owners[msg.sender]);
         return super.transfer(_to, _value);
     }
 
-    //////////////////////
-    //  mint and burn   //
-    //////////////////////
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
     function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
         require(_to != address(0));
         require(_amount >= 0);
@@ -54,6 +91,10 @@ contract PXL is StandardToken, OwnableToken {
         return true;
     }
 
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
     function burn(uint256 _amount) onlyOwner public {
         require(_amount >= 0);
         require(_amount <= balances[msg.sender]);
