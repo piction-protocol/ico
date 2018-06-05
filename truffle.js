47cec938ee4194226cf50a27d3e91523224884a3
sk.kwon <sk.kwon@battleent.com>
2018-06-05 14:28:29 +0900

    Reset truffle.js

--- truffle.js
+++ truffle.js
@@ -1,17 +1,9 @@
-require('dotenv-flow').config({ default_node_env: 'ropsten' });
-var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
-
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
-    ropsten: {
-      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://ropsten.infura.io/"),
-      network_id: 3,
-      gas: 3000000
-    }    
-  }
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    }
 };
