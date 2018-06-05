0b39a5306ad780a3476062db597d8d2a9ad4cf16
sk.kwon <sk.kwon@battleent.com>
2018-06-05 14:28:55 +0900

    Delete migration files

--- migrations/4_deploy_whitelist_contracts.js
+++
@@ -1,8 +0,0 @@
-const colors = require('colors');
-const Whitelist = artifacts.require("Whitelist");
-
-module.exports = async (deployer, network) => {
-    var contract = await Whitelist.new();
-    console.log(colors.red('Deployed Whitelist contract'));
-    console.log(colors.red('Whitelist address : %s'), contract.address);
-};
